b385a633e444dbdb24abb5eb8abc8da76d3cdc88
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-02-25 15:39:28 +0100

    Update pun.c

--- 02/exercises/pun.c
+++ 02/exercises/pun.c
@@ -1,1 +1,11 @@
+// Name: pun.c
+// Purpose: Prints a bad pun.
+// Author: NiceMan1337
+// Date: 24.02.2022
 
+#include <stdio.h>
+
+int main (void) {
+    printf("To C, or not to C: that is a question.\n");
+    return 0;
+}
