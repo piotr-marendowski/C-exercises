728099364c3ff648eeaf58df10354278ae3ba49e
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-02-26 15:28:40 +0100

    Create p2-1.c

---
+++ 02/projects/p2-1.c
@@ -0,0 +1,22 @@
+/*  Name: p2-1.c
+    Purpose: Programming project 1.
+    Author: NiceMan1337
+    Date: 26.02.2022  */
+
+#include <stdio.h>
+
+int main (void)
+{
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+
+    return 0;
+}
+
+/*Description:
+Write by using printf statements, spaces
+and newline characters*/
