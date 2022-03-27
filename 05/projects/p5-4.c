22b704d8d6b659f0eaa4b15d72c7e9979855283a
NiceMan1337 <piotr_marendowski@vp.pl>
2022-03-27 17:15:02 +0200

    Uploaded

---
+++ 05/projects/p5-4.c
@@ -0,0 +1,41 @@
+/*  Name: p5-4.c
+    Purpose: Project 5-4.
+    Author: NiceMan1337.
+    Date: 27.03.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    /*declare variable*/
+    int speed;
+    
+    /*ask user for wind speed*/
+    printf("Enter wind speed (in knots): ");
+    scanf("%d", &speed);
+
+    /*calculate and print out the description*/
+    if (speed < 1) {
+        printf("calm\n");
+    }
+    else if (speed > 1 && speed < 3) {
+        printf("ligth air\n");
+    }
+    else if (speed > 4 && speed < 27) {
+        printf("breeze\n");
+    }
+    else if (speed > 28 && speed < 47) {
+        printf("gale\n");
+    }
+    else if (speed > 48 && speed < 63) {
+        printf("storm!\n");
+    }
+    else if (speed > 63) {
+        printf("Get out of here! It's a hurricane!\n");
+    }
+    
+    return 0;
+}
+
+/*Description:
+You can also see williamgherman's solution*/
---
+++ 05/projects/p5-5.c
@@ -0,0 +1,41 @@
+/*  Name: p5-5.c
+    Purpose: Project 5-5.
+    Author: NiceMan1337.
+    Date: 27.03.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    /*declare variables*/
+    float income;
+    
+    /*ask user for income*/
+    printf("Enter income: ");
+    scanf("%f", &income);
+
+    /*calculate and print out the description*/
+    if (income <= 750.00f) {
+        printf("Your tax is: %.2f\n", income * 0.01f);
+    }
+    else if (income <= 2250.00f) {
+        printf("Your tax is: %.2f\n", 7.50f + ((income - 750.00f) * 0.02f));
+    }
+    else if (income <= 3750.00f) {
+        printf("Your tax is: %.2f\n", 37.50f + ((income - 2250.00f) * 0.03f));
+    }
+    else if (income <= 5250.00f) {
+        printf("Your income is: %.2f\n", 82.50f + ((income - 3750.00f) * 0.04f));
+    }
+    else if (income <= 7000.00f) {
+        printf("Your income is: %.2f\n", 142.50f + ((income - 5250.00f) * 0.05f));
+    }
+    else {
+        printf("Your income is: %.2f\n", 230.00f + ((income - 7000.00f) * 0.06f));
+    }
+    
+    return 0;
+}
+
+/*Description:
+You can also see williamgherman's solution*/
