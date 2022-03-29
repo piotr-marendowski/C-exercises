f66ccd11312f99d7146c5a3ed7ab128a6211388c
NiceMan1337 <piotr_marendowski@vp.pl>
2022-03-29 20:00:32 +0200

    Uploaded

---
+++ 05/projects/p5-7.c
@@ -0,0 +1,47 @@
+/*  Name: p5-6.c
+    Purpose: Project p5-6.
+    Author: williamgherman
+    Date: 28.03.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    int n1, n2, n3, n4, max, min, min1, min2, max1, max2;
+
+    printf("Enter four integers: ");
+    scanf("%d%d%d%d", &n1, &n2, &n3, &n4);
+
+    if (n1 >= n2) {
+        max1 = n1;
+        min1 = n2;
+    } else { 
+        max1 = n2;
+        min1 = n1;
+    }
+    
+    if (n3 >= n4) {
+        max2 = n3;
+        min2 = n4;
+    } else {
+        max2 = n4;
+        min2 = n3;
+    }
+
+    if (max2 > max1)
+        max = max2;
+    else
+        max = max1;
+    if (min1 < min2)
+        min = min1;
+    else
+        min = min2;
+
+    printf("Largest: %d\nSmallest: %d\n", max, min); 
+ 
+    return 0;
+}
+
+/*Description:
+The solutions is from: williamgherman/c-solutions
+because I couldn't get it to work in four statements*/
