49af18a44e1fe5e96fc729d6e029e18f3b3bd7e7
NiceMan1337 <piotr_marendowski@vp.pl>
2022-04-01 20:22:28 +0200

    Uploaded

---
+++ 06/exercises/numdigits.c
@@ -0,0 +1,24 @@
+/*  Name: numdigits.c
+    Purpose: Calculates the number of digits in an integer.
+    Author: K.N. King
+    Date: 01.04.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    int digits = 0, n;
+
+    printf("Enter a nonnegative integer: ");
+    scanf("%d", &n);
+
+    do {
+        n /= 10;
+        digits++;
+    } while (n > 0);
+
+    printf("The number has %d digit(s).\n", digits);
+
+    return 0;
+}
+
