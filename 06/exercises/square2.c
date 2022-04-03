9f652c9704c48f1498e67b41c1e20f5e37c1e167
NiceMan1337 <piotr_marendowski@vp.pl>
2022-04-03 18:36:35 +0200

    Uploaded

---
+++ 06/exercises/square2.c
@@ -0,0 +1,20 @@
+/*  Name: square2.c
+    Purpose: Prints a table of squares using a for statement.
+    Author: K.N. King
+    Date: 03.04.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{ 
+    int i, n;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+        printf("%10d%10d\n", i, i * i);
+
+    return 0;
+}
