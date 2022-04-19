e12a30b8e1e8c21efc68a070c90729315f828f4e
NiceMan1337 <piotr_marendowski@vp.pl>
2022-04-19 17:05:42 +0200

    Updated filenames

---
+++ 02/exercises/2-09.c
@@ -0,0 +1,14 @@
+/*  Name: 2-9.c
+    Purpose: Exercise 9.
+    Author: NiceMan1337
+    Date: 19.04.2022  */
+
+#include <stdio.h>
+
+int main (void)
+{
+    int answer, p;
+    answer = (3 * p - p * p) / 3;
+
+    return 0;
+}
