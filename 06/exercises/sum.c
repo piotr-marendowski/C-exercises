de77d81ecb27ffb4ead1301b6df35a6a4bc3726c
NiceMan1337 <piotr_marendowski@vp.pl>
2022-04-20 20:25:14 +0200

    Updated

--- 06/exercises/sum.c
+++ 06/exercises/sum.c
@@ -7,17 +7,17 @@
 
 int main(void)
 { 
-    int n, sum = 0;
+    long n, sum = 0;
 
     printf("This program sums a series of integers.\n");
     printf("Enter integers (0 to terminate): ");
     
-    scanf("%d", &n);
+    scanf("%ld", &n);
     while (n != 0) {
         sum += n;
-        scanf("%d", &n);
+        scanf("%ld", &n);
     }
-    printf("The sum is: %d\n", sum);
+    printf("The sum is: %ld\n", sum);
     
     return 0;
 }
