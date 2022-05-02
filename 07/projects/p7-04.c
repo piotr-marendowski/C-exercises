5797bfd66b0fd26affa3eb8672eab2cf1c414723
NiceMan1337 <piotr_marendowski@vp.pl>
2022-05-02 15:44:07 +0200

    Uploaded

--- 07/projects/p7-04.c
+++ 07/projects/p7-04.c
@@ -1,27 +1,52 @@
-/*  Name: p7-03.c
-    Purpose: Sums a series of numbers.
+/*  Name: p7-04.c
+    Purpose: Translates an alphabetic phone number into numeric form.
     Author: NiceMan1337
-    Date: 30.04.2022  */
+    Date: 02.05.2022  */
 
 #include <stdio.h>
 
 int main(void)
 { 
-    double n, sum = 0.0;
+    char c;
 
-    printf("This program sums a series of integers.\n");
-    printf("Enter integers (0 to terminate): ");
-    
-    scanf("%lf", &n);
-    while (n != 0.0) {
-        sum += n;
-        scanf("%lf", &n);
+    printf("Enter phone number: ");
+    while ((c = getchar()) != '\n') {
+        switch (c) {
+            case 'A': case 'B': case 'C':
+                putchar('2');
+                break;
+            case 'D': case 'E': case 'F':
+                putchar('3');
+                break;
+            case 'G': case 'H': case 'I':
+                putchar('4');
+                break;
+            case 'J': case 'K': case 'L':
+                putchar('5');
+                break;
+            case 'M': case 'N': case 'O':
+                putchar('6');
+                break;
+            case 'P': case 'R': case 'S':
+                putchar('7');
+                break;
+            case 'T': case 'U': case 'V':
+                putchar('8');
+                break;
+            case 'W': case 'X': case 'Y':
+                putchar('9');
+                break;
+            default:
+                putchar(c);
+                break;
+        }
     }
-    printf("The sum is: %.2f\n", sum);
+    printf("\n");
     
     return 0;
 }
 
 /*Description:
-For scanf you need to add %lf, but printf needs %f,
-in C99 you can write %lf*/
+Solution by: williamgherman/c-solutions
+This solution is what you would do, I tried some
+other ways, without success :/ */
