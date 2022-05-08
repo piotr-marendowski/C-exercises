d0bd71928f5cb19ce554c8bfdb1699a23a1b10a6
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-05-08 13:33:53 +0200

    Uploaded

---
+++ 07/projects/p7-07.c
@@ -0,0 +1,48 @@
+/*  Name: p7-07.c
+    Purpose: Fraction calculator.
+    Author: NiceMan1337
+    Date: 07.05.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{
+    /*declare variables*/
+    int num1, denom1, num2, denom2, result_num, result_denom;
+    char ch;
+
+    /*ask for fractions, use getchar to get type of operation
+    getchar(); is necessary, to NOT load white space from scanf*/
+    printf("Enter two fractions separated by +, -, *, /: ");
+    scanf("%d/ %d", &num1, &denom1);
+    getchar();
+    ch = getchar();
+    scanf("%d/ %d", &num2, &denom2);
+
+    /*calculate, based on operand*/
+    if (ch == '+') {
+        result_num = num1 * denom2 + num2 * denom1;
+        result_denom = denom1 * denom2;
+    }
+    else if (ch == '-') {
+        result_num = num1 * denom2 - num2 * denom1;
+        result_denom = denom1 * denom2;
+    }
+    else if (ch == '*') {
+        result_num = num1 * num2;
+        result_denom = denom1 * denom2;
+    }
+    else if (ch == '/') {
+        result_num = num1 * denom2;
+        result_denom = denom1 * num2;
+    }
+    
+    /*print the result*/
+    printf("The result is %d/%d\n", result_num, result_denom);
+
+    return 0;
+}
+
+/*Description:
+williamgherman combined it with GCD calculator,
+to get the simpliest result*/
---
+++ 07/projects/p7-08.c
@@ -0,0 +1,62 @@
+/*  Name: p7-08.c
+    Purpose: 12-hour time flight departure calculator.
+    Author: NiceMan1337
+    Date: 08.05.2022  */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{ 
+    /*declare variables, f1 = flight 1*/
+    int time, hour, minute, f1 = 480, f2 = 583, f3 = 679,
+     f4 = 767, f5 = 840, f6 = 945, f7 = 1140, f8 = 1305;
+    char ch1, ch2;
+
+    /*ask user for a time*/
+    printf("Enter a 12-hour time: ");
+    scanf("%d :%d %c%c", &hour, &minute, &ch1, &ch2);
+
+    /*convert input into minutes after midnight
+    if it's a PM time, add 12 hours to for calculation*/
+    time = hour * 60 + minute;
+    if (toupper(ch1) == 'P') { 
+        time += 720; /*60 mins * 12 hours = 720*/
+    }
+    /*calculate and print out result*/
+    printf("Closest departure time is ");
+
+    /*calculate difference between first flight and second flight
+    then find middle point by dividing them by 2
+    and add this to first flight
+    and you will get first half for first flight
+    and second half for second flight
+    then do it again for others*/
+
+    if (time <= f1 + (f2 - f1) / 2) {
+        printf("8:00 a.m., arriving at 10:16 a.m.\n");
+    }
+    else if (time < f2 + (f3 - f2) / 2) {
+        printf("9:43 a.m., arriving at 11:52 a.m.\n");
+    }
+    else if (time < f3 + (f4 - f3) / 2) {
+        printf("11:19 a.m., arriving at 1:31 p.m.\n");
+    }
+    else if (time < f4 + (f5 - f4) / 2) {
+        printf("12:47 p.m., arriving at 3:00 p.m.\n");
+    }
+    else if (time < f5 + (f6 - f5) / 2) {
+        printf("2:00 p.m., arriving at 4:08 p.m.\n");
+    }
+    else if (time < f6 + (f7 - f6) / 2) {
+        printf("3:45 p.m., arriving at 5:55 p.m.\n");
+    }
+    else if (time < f7 + (f8 - f7) / 2) {
+        printf("7:00 p.m., arriving at 9:20 p.m.\n");
+    }
+    else {
+        printf("9:45 p.m., arriving at 11:58 p.m.\n");
+    }
+ 
+    return 0;
+}
