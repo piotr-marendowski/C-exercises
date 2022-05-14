898b877b3234436a13b018ea5ed74a08fead6368
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-05-14 14:14:27 +0200

    Upload

---
+++ 07/projects/p7-14.c
@@ -0,0 +1,35 @@
+/*  Name: p7-14.c
+    Purpose: Computes Newton's method to compute the
+    square root of a positive floating-point number.
+    Author: NiceMan1337
+    Date: 14.05.2022  */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+    /*declare variables*/
+    int x;
+    double y, dif, avg = 1.0;
+
+    /*ask for a number*/ 
+    printf("Enter a positive number: ");
+    scanf("%d", &x);
+
+    /*calcualate the square root*/
+    while (dif != 1) {
+        y = avg;
+        avg = ((x / y) + y) / 2;
+        dif = fabs(y / avg);
+    }
+    
+    /*print the result*/
+    printf("Square root: %.5f\n", y);
+
+    return 0;
+}
+
+/*Description:
+That's the easier way to do this, but not perfectly what author wanted,
+williamgherman/c-solutions - for more advanced solution */
---
+++ 07/projects/p7-15.c
@@ -0,0 +1,35 @@
+/*  Name: p7-15.c
+    Purpose: Computes factorial from a positive integer
+    Author: NiceMan1337
+    Date: 14.05.2022  */
+
+#include <stdio.h>
+
+int main(void) {
+
+    /*declare variables*/
+    int num, i;
+    short factorial = 1;
+
+    /*ask for a positive integer*/
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    /*calculate the factorial*/
+    for (i = num; i > 1; i--)
+        factorial *= i;
+
+    /*print the result*/
+    printf("Factorial of %d: %d\n", num, factorial);
+
+    return 0;
+}
+/*Solution:
+It depends on what machine code is running,
+in my case:
+(a) 7
+(b) 13
+(c) 20
+(e) 34
+(f) 170
+compiled on C89 */
