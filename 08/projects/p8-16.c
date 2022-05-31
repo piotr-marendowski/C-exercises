361fbedcd95cda1749073bd06070e071ef4b49da
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-05-31 19:31:26 +0200

    Uploaded

---
+++ 08/projects/p8-16.c
@@ -0,0 +1,48 @@
+/*  Name: p8-16.c
+    Purpose: Check if two words are anagrams.
+    Author: NiceMan1337
+    Date: 31.05.2022  */
+
+#include <stdio.h>
+/*for tolower and isalpha*/
+#include <ctype.h>
+
+int main(void)
+{ 
+    /*declare variables*/
+    int i, word[26] = {0};
+    char ch;
+
+    /*ask for first word and count letters*/
+    printf("Enter first word: ");
+    while ((ch = getchar()) != '\n') {
+        ch = tolower(ch);
+        /*if ch is in alphabet*/
+        if (isalpha(ch)) {
+            word[ch - 'a']++;
+        }
+    }
+
+    /*ask for second word and "uncount" letters in array*/
+    printf("Enter second word: ");
+    while ((ch = getchar()) != '\n') {
+        ch = tolower(ch);
+        /*if ch is in alphabet*/
+        if (isalpha(ch)) {
+            word[ch - 'a']--;
+        }
+    }
+
+    /*print the result
+    if any letter is not same, print not and break*/
+    printf("The words are ");
+    for (i = 0; i < 26; i++) {
+        if (word[i] > 0) {
+            printf("not ");
+            break;
+        }
+    }
+    printf("anagrams.\n");
+    
+    return 0;
+}
