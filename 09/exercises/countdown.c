bb640655b21e1a3a3e646395218eb53b4a08b2e9
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-06-06 18:48:54 +0200

    Uploaded

---
+++ 09/exercises/countdown.c
@@ -0,0 +1,20 @@
+/*  Name: countdown.c
+    Purpose: Prints a countdown.
+    Author: K.N. King
+    Date: 06.06.2022  */
+
+#include <stdio.h>
+
+void print_count(int n) {
+    printf("T minus %d and counting\n", n);
+}
+
+int main(void)
+{ 
+    int i;
+
+    for (i = 10; i > 0; --i)
+        print_count(i); 
+
+    return 0;
+}
---
+++ 09/exercises/pun2.c
@@ -0,0 +1,16 @@
+/*  Name: pun2.c
+    Purpose: Prints a bad pun.
+    Author: K.N. King
+    Date: 06.06.2022  */
+
+#include <stdio.h>
+
+void print_pun(void)
+{
+    printf("To C, or not to C: that is a question.\n");
+}
+
+int main(void) {
+    print_pun();
+    return 0;
+}
