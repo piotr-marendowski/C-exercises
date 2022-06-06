9ce3a7620671c7a2d1c49e81ede1bd7e029955d8
NiceMan1337 <37738748+NiceMan1337@users.noreply.github.com>
2022-06-06 18:48:22 +0200

    Uploaded

---
+++ 09/exercises/average.c
@@ -0,0 +1,23 @@
+/*  Name: average.c
+    Purpose: Computes pairwise averages of three numbers.
+    Author: K.N. King
+    Date: 06.06.2022  */
+
+#include <stdio.h>
+
+double average(double a, double b) {
+    return (a + b) / 2;
+}
+
+int main(void)
+{ 
+    double x, y, z;
+
+    printf("Enter three numbers: ");
+    scanf("%lf%lf%lf", &x, &y, &z);
+    printf("Average of %g and %g: %g\n", x, y, average(x, y));
+    printf("Average of %g and %g: %g\n", y, z, average(y, z));
+    printf("Average of %g and %g: %g\n", x, z, average(x, z));
+
+    return 0;
+}
