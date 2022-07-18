cc477e23df62757e568ff3e2e523c408bcaf908f
NiceMan1337 <piotr_marendowski@vp.pl>
2022-07-18 10:12:46 +0200

    Uploaded

---
+++ 11/exercises/11-01.c
@@ -0,0 +1,14 @@
+/*  Name: 11-01.c
+    Purpose: Exercise 11-01.
+    Author: NiceMan1337
+    Date: 18.07.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
+
+/*Solution:
+(a) and (g) are aliases for i. */
---
+++ 11/exercises/11-02.c
@@ -0,0 +1,14 @@
+/*  Name: 11-02.c
+    Purpose: Exercise 11-02.
+    Author: NiceMan1337
+    Date: 18.07.2022  */
+
+#include <stdio.h>
+
+int main(void)
+{
+    return 0;
+}
+
+/*Solution:
+(e), (f) and (i) are legal. */
