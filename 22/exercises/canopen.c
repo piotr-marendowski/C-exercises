e27c277da96ea48bcfc84ccd8bfd0957bf339346
Piotr Marendowski <piotr_marendowski@vp.pl>
2023-01-18 19:49:53 +0000

    Upload

---
+++ 22/exercises/canopen.c
@@ -0,0 +1,25 @@
+/* Author: K.N. King
+Purpose: Checks whether a file can be opened for reading
+Modification date (DD/MM/YYYY): 18.01.2022 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+
+    if (argc != 2) {
+        printf("usage: canopen filename\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((fp = fopen(argv[1], "r")) == NULL) {
+        printf("%s can't be opened\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%s can be opened\n", argv[1]);
+    fclose(fp);
+    return 0;
+}
