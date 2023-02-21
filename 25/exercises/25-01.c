fd7971808a3accd01cb7e8eedc5d29e9fb94fb3e
Piotr Marendowski <piotr-marendowski@tutanota.com>
2023-02-21 21:25:25 +0000

    Upload ch 25

---
+++ 25/exercises/25-01.c
@@ -0,0 +1,19 @@
+/* License: BSD-3-Clause license
+Author: Piotr Marendowski
+Purpose: Exercise 1
+Modification date (DD/MM/YYYY): 21.02.2023 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+    Well, on my Arch machine i got only:
+    C
+    C.UTF-8
+    en_US.utf8
+    POSIX
+    */
+
+    return 0;
+}
